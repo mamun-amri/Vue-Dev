22510983011c6d8a705f2bcb9a1aedcd13c03466
mamun-amri <mamunamri97@gmail.com>
2025-02-24 14:19:08 +0700

    feat: tutorial vue

--- vue-dasar/src/layouts/Login.vue
+++ vue-dasar/src/layouts/Login.vue
@@ -3,7 +3,7 @@
     <div
       class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
     >
-      <a
+      <!-- <a
         href="#"
         class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
       >
@@ -13,7 +13,7 @@
           alt="logo"
         />
         Flowbite
-      </a>
+      </a> -->
       <div
         class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
       >
@@ -31,9 +31,10 @@
                 >Your email</label
               >
               <input
-                type="email"
-                name="email"
-                id="email"
+                @change="inputField"
+                type="text"
+                name="username"
+                id="username"
                 class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="name@company.com"
                 required=""
@@ -46,6 +47,7 @@
                 >Password</label
               >
               <input
+                @change="inputField"
                 type="password"
                 name="password"
                 id="password"
